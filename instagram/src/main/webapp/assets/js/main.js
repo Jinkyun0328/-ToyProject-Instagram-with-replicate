d44b7c5a5e6931e6e34d2b265d8cd108281d5702
Jinkyun0328 <123911778+Jinkyun0328@users.noreply.github.com>
2023-12-12 10:11:17 +0900

    clone coding

--- instagram/src/main/webapp/assets/js/main.js
+++ instagram/src/main/webapp/assets/js/main.js
@@ -1,352 +1,187 @@
-/*
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function($) {
-
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
-
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = (skel.vars.browser == 'ie' || skel.vars.browser == 'edge' || skel.vars.mobile) ? function() { return $(this) } : function(intensity) {
-
-		var	$window = $(window),
-			$this = $(this);
-
-		if (this.length == 0 || intensity === 0)
-			return $this;
-
-		if (this.length > 1) {
-
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
-
-			return $this;
-
-		}
-
-		if (!intensity)
-			intensity = 0.25;
-
-		$this.each(function() {
-
-			var $t = $(this),
-				on, off;
-
-			on = function() {
-
-				$t.css('background-position', 'center 100%, center 100%, center 0px');
-
-				$window
-					.on('scroll._parallax', function() {
-
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-
-						$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
-
-					});
-
-			};
-
-			off = function() {
-
-				$t
-					.css('background-position', '');
-
-				$window
-					.off('scroll._parallax');
-
-			};
-
-			skel.on('change', function() {
-
-				if (skel.breakpoint('medium').active)
-					(off)();
-				else
-					(on)();
-
-			});
-
-		});
-
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
-			});
-
-		return $(this);
-
-	};
-
-	$(function() {
-
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper'),
-			$header = $('#header'),
-			$banner = $('#banner');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load pageshow', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Clear transitioning state on unload/hide.
-			$window.on('unload pagehide', function() {
-				window.setTimeout(function() {
-					$('.is-transitioning').removeClass('is-transitioning');
-				}, 250);
-			});
-
-		// Fix: Enable IE-only tweaks.
-			if (skel.vars.browser == 'ie' || skel.vars.browser == 'edge')
-				$body.addClass('is-ie');
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Scrolly.
-			$('.scrolly').scrolly({
-				offset: function() {
-					return $header.height() - 2;
-				}
-			});
-
-		// Tiles.
-			var $tiles = $('.tiles > article');
-
-			$tiles.each(function() {
-
-				var $this = $(this),
-					$image = $this.find('.image'), $img = $image.find('img'),
-					$link = $this.find('.link'),
-					x;
-
-				// Image.
-
-					// Set image.
-						$this.css('background-image', 'url(' + $img.attr('src') + ')');
-
-					// Set position.
-						if (x = $img.data('position'))
-							$image.css('background-position', x);
-
-					// Hide original.
-						$image.hide();
-
-				// Link.
-					if ($link.length > 0) {
-
-						$x = $link.clone()
-							.text('')
-							.addClass('primary')
-							.appendTo($this);
-
-						$link = $link.add($x);
-
-						$link.on('click', function(event) {
-
-							var href = $link.attr('href');
-
-							// Prevent default.
-								event.stopPropagation();
-								event.preventDefault();
-
-							// Start transitioning.
-								$this.addClass('is-transitioning');
-								$wrapper.addClass('is-transitioning');
-
-							// Redirect.
-								window.setTimeout(function() {
-
-									if ($link.attr('target') == '_blank')
-										window.open(href);
-									else
-										location.href = href;
-
-								}, 500);
-
-						});
-
-					}
-
-			});
-
-		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
-
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
-
-				$window.on('resize', function() {
-					$window.trigger('scroll');
-				});
-
-				$window.on('load', function() {
-
-					$banner.scrollex({
-						bottom:		$header.height() + 10,
-						terminate:	function() { $header.removeClass('alt'); },
-						enter:		function() { $header.addClass('alt'); },
-						leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-					});
-
-					window.setTimeout(function() {
-						$window.triggerHandler('scroll');
-					}, 100);
-
-				});
-
-			}
-
-		// Banner.
-			$banner.each(function() {
-
-				var $this = $(this),
-					$image = $this.find('.image'), $img = $image.find('img');
-
-				// Parallax.
-					$this._parallax(0.275);
-
-				// Image.
-					if ($image.length > 0) {
-
-						// Set image.
-							$this.css('background-image', 'url(' + $img.attr('src') + ')');
-
-						// Hide original.
-							$image.hide();
-
-					}
-
-			});
-
-		// Menu.
-			var $menu = $('#menu'),
-				$menuInner;
-
-			$menu.wrapInner('<div class="inner"></div>');
-			$menuInner = $menu.children('.inner');
-			$menu._locked = false;
-
-			$menu._lock = function() {
-
-				if ($menu._locked)
-					return false;
-
-				$menu._locked = true;
-
-				window.setTimeout(function() {
-					$menu._locked = false;
-				}, 350);
-
-				return true;
-
-			};
-
-			$menu._show = function() {
-
-				if ($menu._lock())
-					$body.addClass('is-menu-visible');
-
-			};
-
-			$menu._hide = function() {
-
-				if ($menu._lock())
-					$body.removeClass('is-menu-visible');
-
-			};
-
-			$menu._toggle = function() {
-
-				if ($menu._lock())
-					$body.toggleClass('is-menu-visible');
-
-			};
-
-			$menuInner
-				.on('click', function(event) {
-					event.stopPropagation();
-				})
-				.on('click', 'a', function(event) {
-
-					var href = $(this).attr('href');
-
-					event.preventDefault();
-					event.stopPropagation();
-
-					// Hide.
-						$menu._hide();
-
-					// Redirect.
-						window.setTimeout(function() {
-							window.location.href = href;
-						}, 250);
-
-				});
-
-			$menu
-				.appendTo($body)
-				.on('click', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					$body.removeClass('is-menu-visible');
-
-				})
-				.append('<a class="close" href="#menu">Close</a>');
-
-			$body
-				.on('click', 'a[href="#menu"]', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Toggle.
-						$menu._toggle();
-
-				})
-				.on('click', function(event) {
-
-					// Hide.
-						$menu._hide();
-
-				})
-				.on('keydown', function(event) {
-
-					// Hide on escape.
-						if (event.keyCode == 27)
-							$menu._hide();
-
-				});
-
-	});
-
-})(jQuery);
+const $inputChat = document.getElementsByClassName("input-chat")[0];
+const $btnChat = document.getElementsByClassName("btn-chat")[0];
+const $commentBox = document.getElementsByClassName("box-comment")[0];
+
+// 댓글 쓰기
+const leaveComment = function (e) {
+  e.preventDefault();
+  const comment = document.createElement("p");
+  const wrapComment = document.createElement("div");
+  comment.textContent = $inputChat.value;
+  wrapComment.appendChild(comment);
+  comment.classList.add("text-comment");
+  $inputChat.value = "";
+
+  const likeIcon = document.createElement("img");
+  likeIcon.src = "./src/icon/heart.png";
+  likeIcon.width = 14;
+  likeIcon.height = 14;
+  likeIcon.classList.add("img-heart-comment");
+  wrapComment.appendChild(likeIcon);
+
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "./src/icon/delete.png";
+  deleteIcon.width = 10;
+  deleteIcon.height = 10;
+  deleteIcon.classList.add("img-delete-comment");
+  wrapComment.appendChild(deleteIcon);
+
+  $commentBox.appendChild(wrapComment);
+};
+
+const visibleButton = function () {
+  if ($inputChat.value.length > 0) {
+    $btnChat.classList.add("btn-chat-effect");
+  } else {
+    $btnChat.classList.remove("btn-chat-effect");
+  }
+};
+
+$btnChat.addEventListener("click", leaveComment);
+$inputChat.addEventListener("keyup", visibleButton);
+
+// 하트 & 지우기
+const controlImg = function (e) {
+  if (e.target.getAttribute("src") == "./src/icon/heart.png") {
+    e.target.setAttribute("src", "./src/icon/fullHeart.png");
+  } else if (e.target.getAttribute("src") == "./src/icon/fullHeart.png") {
+    e.target.setAttribute("src", "./src/icon/heart.png");
+  }
+  if (e.target.className === "img-delete-comment") {
+    let parent = e.target.parentElement;
+    parent.remove();
+  }
+};
+$commentBox.addEventListener("click", controlImg);
+
+// 아이디 찾기
+const $searchBar = document.getElementsByClassName("input-searchBar")[0];
+const $filteredBox = document.getElementsByClassName("box-filteredId")[0];
+const idData = [
+  {
+    userId: "apple_01",
+    userName: "새콤달콤",
+    userImg: "./src/img/profile/apple.jpg",
+  },
+  {
+    userId: "applepie0101",
+    userName: "파이는3.14",
+    userImg: "./src/img/profile/applepie.jpg",
+  },
+  {
+    userId: "peach_01",
+    userName: "어피치가되고싶어",
+    userImg: "./src/img/profile/peach.jpg",
+  },
+  {
+    userId: "peachcookie123",
+    userName: "스윗복숭",
+    userImg: "./src/img/profile/peach_2.jpg",
+  },
+  {
+    userId: "watermelon_03",
+    userName: "수박수박수",
+    userImg: "./src/img/profile/watermelon.jpg",
+  },
+  {
+    userId: "water33",
+    userName: "삼다수",
+    userImg: "./src/img/profile/water.jpg",
+  },
+  {
+    userId: "lemon_04",
+    userName: "레몽몽",
+    userImg: "./src/img/profile/lemon.jpg",
+  },
+  {
+    userId: "lemonpie4040",
+    userName: "레몬파이",
+    userImg: "./src/img/profile/lemon.jpg",
+  },
+  {
+    userId: "candy_0426",
+    userName: "사탕해",
+    userImg: "./src/img/profile/candy.jpg",
+  },
+  {
+    userId: "cake2626",
+    userName: "생일이다",
+    userImg: "./src/img/profile/cake.jpg",
+  },
+];
+
+const searchId = function () {
+  let result = idData.filter((v, i) => {
+    return v.userId.includes($searchBar.value);
+  });
+  result.forEach((v, i) => {
+    const wrapFiltered = document.createElement("div");
+    const wrapText = document.createElement("div");
+    const filteredImg = document.createElement("img");
+    const filteredId = document.createElement("span");
+    const filteredName = document.createElement("span");
+    filteredImg.classList.add("filteredImg");
+    wrapFiltered.classList.add("wrapFiltered");
+    filteredId.classList.add("filteredId");
+    filteredName.classList.add("filteredName");
+    wrapText.classList.add("wrapText");
+
+    wrapFiltered.appendChild(filteredImg);
+    filteredImg.src = v.userImg;
+
+    filteredId.innerText = v.userId;
+    wrapText.appendChild(filteredId);
+
+    filteredName.innerText = v.userName;
+    wrapText.appendChild(filteredName);
+    wrapFiltered.appendChild(wrapText);
+
+    $filteredBox.appendChild(wrapFiltered);
+  });
+  if ($searchBar.value.length === 0) {
+    eraseSearchId();
+    const boxRecentSearch = document.createElement("div");
+    const recentSearchTitle = document.createElement("p");
+    const recentSearchContent = document.createElement("p");
+    recentSearchTitle.innerText = "최근 검색 항목";
+    recentSearchTitle.classList.add("title-recentSearch");
+    recentSearchContent.innerText = "최근 검색 내역 없음.";
+    recentSearchContent.classList.add("content-recentSearch");
+    boxRecentSearch.appendChild(recentSearchTitle);
+    boxRecentSearch.appendChild(recentSearchContent);
+    $filteredBox.appendChild(boxRecentSearch);
+  }
+};
+
+const eraseSearchId = function () {
+  while ($filteredBox.hasChildNodes()) {
+    $filteredBox.removeChild($filteredBox.firstChild);
+  }
+};
+
+$searchBar.addEventListener("keyup", searchId);
+$searchBar.addEventListener("keydown", eraseSearchId);
+$searchBar.addEventListener("click", () => {
+  $filteredBox.style.display = "inline-block";
+});
+$searchBar.addEventListener("focusout", () => {
+  $filteredBox.style.display = "none";
+  $searchBar.value = "";
+});
+
+// 메뉴박스
+const $wrapMenu = document.getElementsByClassName("wrap-menu")[0];
+const $profileImg = document.getElementsByClassName("img-navProfile")[0];
+const $body = document.getElementsByTagName("body")[0];
+const controlMenu = function (e) {
+  if (e.target.className === "img-navProfile") {
+    if ($wrapMenu.style.display === "none") {
+      $wrapMenu.style.display = "inline-block";
+    } else {
+      $wrapMenu.style.display = "none";
+    }
+  } else {
+    $wrapMenu.style.display = "none";
+  }
+};
+$body.addEventListener("click", controlMenu);
